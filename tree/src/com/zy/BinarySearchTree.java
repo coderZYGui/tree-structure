7fde7421455328d70b0d227374c2fb1a3cc45def
zygui_g <zygui@163.com>
2020-04-15 21:15:25 +0800

    二叉排序树-接口设计

---
+++ tree/src/com/zy/BinarySearchTree.java
@@ -0,0 +1,38 @@
+package com.zy;
+
+/**
+ * Description: 二叉排序树实现
+ *
+ * @author zygui
+ * @date 2020/4/15 21:08
+ */
+public class BinarySearchTree<E> {
+
+    private int size;
+
+    public int size(){
+        return 0;
+    }
+
+    public boolean isEmpty(){
+        return false;
+    }
+
+    public void clear(){
+
+    }
+
+    public void add(E element){
+
+    }
+
+    public void remove(E element){
+
+    }
+
+    public boolean contains(E element){
+        return false;
+    }
+
+
+}
---
+++ tree/src/com/zy/Main.java
@@ -0,0 +1,13 @@
+package com.zy;
+
+/**
+ * Description:
+ *
+ * @author zygui
+ * @date 2020/4/15 21:08
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
