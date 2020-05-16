2068440ef613338d5b4f109448ecda11764adda9
zygui_g <zygui@163.com>
2020-05-16 16:48:54 +0800

    测试打印Person类型元素的二叉树

--- tree/src/com/zy/Main.java
+++ tree/src/com/zy/Main.java
@@ -10,39 +10,53 @@ import com.zy.printer.BinaryTrees;
  */
 public class Main {
     public static void main(String[] args) {
-        Integer data[] = new Integer[] {
-            7, 4, 9, 2, 5, 8, 11, 3, 12
-        };
 
-        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        test2();
+    }
+
+    /**
+     * 测试用例2: 二叉树传入Person类型
+     */
+    public static void test2() {
+        // 此时这里就没有使用自定义的比较器了,使用的是Person内部实现CompareTo的比较规则
+        Integer data[] = new Integer[]{
+                7, 4, 9, 2, 5, 8, 11, 3, 12, 1
+        };
+        BinarySearchTree<Person> bst2 = new BinarySearchTree<>();
         for (int i = 0; i < data.length; i++) {
-            bst.add(data[i]);
+            bst2.add(new Person(data[i]));
         }
 
-        BinaryTrees.println(bst);
+        BinaryTrees.println(bst2);
 
-        // 此时这里就没有使用自定义的比较器了,使用的是Person内部实现CompareTo的比较规则
-//        BinarySearchTree<Person> bst2 = new BinarySearchTree<>();
-//        bst2.add(new Person(12));
-//        bst2.add(new Person(15));
-//
-//        // 这里使用的是匿名内部类来实现接口,并重写接口的方法
-//        BinarySearchTree<Person> bst3 = new BinarySearchTree<>(new Comparator<Person>() {
-//            @Override
-//            public int compare(Person e1, Person e2) {
-//                return e2.getAge() - e1.getAge();
-//            }
-//        });
-//        bst3.add(new Person(12));
-//        bst3.add(new Person(15));
-//    }
-//
-//    // 创建不同规则的比较器(年龄大的,属于大的元素)
-//    private static class PersonComparator1 implements Comparator<Person> {
-//
-//        @Override
-//        public int compare(Person e1, Person e2) {
-//            return e1.getAge() - e2.getAge();
-//        }
+        System.out.println("---------------------------------------------");
+
+        BinarySearchTree<Person> bst3 = new BinarySearchTree<>(new Comparator<Person>() {
+            @Override
+            public int compare(Person e1, Person e2) {
+                return e2.getAge() - e1.getAge();
+            }
+        });
+        for (int i = 0; i < data.length; i++) {
+            bst3.add(new Person(data[i]));
+        }
+
+        BinaryTrees.println(bst3);
+    }
+
+    /**
+     * 测试用例1: 二叉树传入Integer类型; 扩展: 内置的一些类,已经默认实现Comparable<T>接口,并实现了compareTo方法
+     */
+    public static void test1() {
+        Integer data[] = new Integer[]{
+                7, 4, 9, 2, 5, 8, 11, 3, 12, 1
+        };
+
+        BinarySearchTree<Integer> bst1 = new BinarySearchTree<>();
+        for (int i = 0; i < data.length; i++) {
+            bst1.add(data[i]);
+        }
+
+        BinaryTrees.println(bst1);
     }
 }
--- tree/src/com/zy/Person.java
+++ tree/src/com/zy/Person.java
@@ -6,7 +6,7 @@ package com.zy;
  * @author zygui
  * @date 2020/5/16 14:41
  */
-public class Person implements Comparable<Person>{
+public class Person implements java.lang.Comparable<Person> {
     private int age;
 
     public Person(int age) {
@@ -27,5 +27,10 @@ public class Person implements Comparable<Person>{
 //        if (age < e.age) return -1;
 //        return 0;
         return age - e.age; // 相当于上面的三行代码
+    }
+
+    @Override
+    public String toString() {
+        return "age=" + this.age + "";
     }
 }
