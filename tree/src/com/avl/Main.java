b3497eb38356b67eecd22be4c61211f08644abe8
guizy <coderzygui@163.com>
2020-11-20 23:51:09 +0800

    AVLTree-统一旋转操作

--- tree/src/com/avl/Main.java
+++ tree/src/com/avl/Main.java
@@ -19,11 +19,14 @@ public class Main {
                 85, 19, 69, 3, 7, 99, 95, 2, 1, 70, 44, 58, 11, 21, 14, 93, 57, 4, 56
         };
 
-        AVLTree<Integer> bst = new AVLTree<>();
-        for (Integer datum : data) {
-            bst.add(datum);
+        AVLTree<Integer> avl = new AVLTree<>();
+        for (int i = 0; i < data.length; i++) {
+            avl.add(data[i]);
+            System.out.println("[" + data[i] + "]");
+            BinaryTrees.println(avl);
+            System.out.println("-----------------------------------------------------------------------------------------------------------------------------");
         }
 
-        BinaryTrees.println(bst);
+        // BinaryTrees.println(avl);
     }
 }
--- tree/src/com/avl/tree/AVLTree.java
+++ tree/src/com/avl/tree/AVLTree.java
@@ -59,11 +59,11 @@ public class AVLTree<E> extends BST<E> {
     }
 
     /**
-     * 恢复平衡
+     * 恢复平衡-分LL、LR、RL、RR来旋转
      *
      * @param grand 高度最低的那个不平衡节点
      */
-    private void rebalance(Node<E> grand) {
+    private void rebalance2(Node<E> grand) {
         Node<E> parent = ((AVLNode<E>) grand).tallerChild();
         Node<E> node = ((AVLNode<E>) parent).tallerChild();
         if (parent.isLeftChild()) { // L
@@ -81,6 +81,70 @@ public class AVLTree<E> extends BST<E> {
                 rotateLeft(grand);
             }
         }
+    }
+
+    /**
+     * 恢复平衡-统一旋转
+     *
+     * @param grand 高度最低的那个不平衡节点
+     */
+    private void rebalance(Node<E> grand) {
+        Node<E> parent = ((AVLNode<E>) grand).tallerChild();
+        Node<E> node = ((AVLNode<E>) parent).tallerChild();
+        if (parent.isLeftChild()) { // L
+            if (node.isLeftChild()) { // LL
+                rotate(grand, node.left, node, node.right, parent, parent.right, grand, grand.right);
+            } else { // LR
+                rotate(grand, parent.left, parent, node.left, node, node.right, grand, grand.right);
+            }
+        } else { // R
+            if (node.isLeftChild()) { // RL
+                rotate(grand, grand.left, grand, node.left, node, node.right, parent, parent.right);
+            } else { // RR
+                rotate(grand, grand.left, grand, parent.left, parent, node.left, node, node.right);
+            }
+        }
+    }
+
+    /**
+     * 统一旋转
+     */
+    private void rotate(
+            Node<E> r, // 子树的根节点
+            Node<E> a, Node<E> b, Node<E> c,
+            Node<E> d,
+            Node<E> e, Node<E> f, Node<E> g) {
+        // 让d成为这颗子树的根节点
+        d.parent = r.parent;
+        if (r.isLeftChild()) {
+            r.parent.left = d;
+        } else if (r.isRightChild()) {
+            r.parent.right = d;
+        } else {
+            root = d;
+        }
+
+        // a-b-c
+        b.left = a;
+        if (a != null) a.parent = b;
+        b.right = c;
+        if (c != null) c.parent = b;
+        // 更新b的高度, 因为b的左右子树都变了, 所以要更新高度
+        updateHeight(b);
+
+        // e-f-g
+        f.left = e;
+        if (e != null) e.parent = f;
+        f.right = g;
+        if (g != null) g.parent = f;
+        updateHeight(f);
+
+        // b-d-f
+        d.left = b;
+        d.right = f;
+        b.parent = d;
+        f.parent = d;
+        updateHeight(d);
     }
 
     /**
