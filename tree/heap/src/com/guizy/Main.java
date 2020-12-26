468a86c10b12c75df3cababc1b941ad0f17a47b5
guizy <coderzygui@163.com>
2020-12-26 22:36:12 +0800

    BinaryHeap-根据compare来实现小顶堆

--- tree/heap/src/com/guizy/Main.java
+++ tree/heap/src/com/guizy/Main.java
@@ -14,13 +14,20 @@ public class Main {
 
     public static void main(String[] args) {
         Integer[] data = {88, 44, 53, 41, 16, 6, 70, 18, 85, 98, 81, 23, 36, 43, 37};
+        BinaryHeap<Integer> heap = new BinaryHeap<>(data, (o1, o2) -> o1 - o2);
+        BinaryTrees.println(heap);
+
+    }
+
+    @Test
+    public void test1() {
+        Integer[] data = {88, 44, 53, 41, 16, 6, 70, 18, 85, 98, 81, 23, 36, 43, 37};
         BinaryHeap<Integer> heap = new BinaryHeap<>(data);
         BinaryTrees.println(heap);
 
         data[0] = 10;
         data[1] = 20;
         BinaryTrees.println(heap);
-
     }
 
     @Test
