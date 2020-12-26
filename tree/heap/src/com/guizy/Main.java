9445dc94efd7acc1e80601a1f11d08e9527f7c37
guizy <coderzygui@163.com>
2020-12-26 17:35:30 +0800

    BinaryHeap-因为除了二叉堆,还有很对堆结构, 比如斐波那契堆等; 为了通用, 我们可以抽取一个堆父类

--- tree/heap/src/com/guizy/Main.java
+++ tree/heap/src/com/guizy/Main.java
@@ -1,5 +1,8 @@
 package com.guizy;
 
+import com.guizy.heap.BinaryHeap;
+import com.guizy.printer.BinaryTrees;
+
 /**
  * Description:
  *
@@ -7,4 +10,16 @@ package com.guizy;
  * @date 2020/12/25 17:29
  */
 public class Main {
+    public static void main(String[] args) {
+
+        BinaryHeap<Integer> heap = new BinaryHeap<>();
+        heap.add(68);
+        heap.add(72);
+        heap.add(43);
+        heap.add(50);
+        heap.add(38);
+        heap.add(888);
+
+        BinaryTrees.print(heap);
+    }
 }
--- tree/heap/src/com/guizy/heap/BinaryHeap.java
+++ tree/heap/src/com/guizy/heap/BinaryHeap.java
@@ -1,5 +1,7 @@
 package com.guizy.heap;
 
+import com.guizy.printer.BinaryTreeInfo;
+
 import java.util.Comparator;
 
 /**
@@ -9,7 +11,7 @@ import java.util.Comparator;
  * @date 2020/12/25 17:33
  */
 @SuppressWarnings("unchecked")
-public class BinaryHeap<E> implements Heap<E> {
+public class BinaryHeap<E> implements Heap<E>, BinaryTreeInfo {
     private E[] elements;
     private int size;
     private Comparator<E> comparator;
@@ -18,6 +20,10 @@ public class BinaryHeap<E> implements Heap<E> {
     public BinaryHeap(Comparator<E> comparator) {
         this.comparator = comparator;
         this.elements = (E[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    public BinaryHeap() {
+        this(null);
     }
 
     @Override
@@ -43,6 +49,7 @@ public class BinaryHeap<E> implements Heap<E> {
         elementNotNullCheck(element);
         ensureCapacity(size + 1);
         elements[size++] = element;
+        // 上滤,当添加一个节点, 要恢复二叉堆的性质
         siftUp(size - 1);
     }
 
@@ -68,20 +75,34 @@ public class BinaryHeap<E> implements Heap<E> {
      * @param index
      */
     private void siftUp(int index) {
+//        E e = elements[index];
+//        // 必须要存在父节点, index > 0表示有父节点
+//        while (index > 0) {
+//            // 获取父节点的索引. 根据公式, (index-1)/2
+//            int pindex = (index - 1) >> 1;
+//            E p = elements[pindex];
+//            // 插入的节点 <= 父节点, 此时符合二叉堆性质,退出
+//            if (compare(e, p) <= 0) return;
+//            // 交换index, pindex位置的内容
+//            E tmp = elements[index];
+//            elements[index] = elements[pindex];
+//            elements[pindex] = tmp;
+//            // 重新赋值(继续上滤),循环判断
+//            index = pindex;
+//        }
+
+        // 对比上面, 进行优化
         E e = elements[index];
-        // 必须要存在父节点
         while (index > 0) {
-            // 获取父节点的索引
             int pindex = (index - 1) >> 1;
             E p = elements[pindex];
-            if (compare(e, p) <= 0) return;
-            // 交换index, pindex位置的内容
-            E tmp = elements[index];
-            elements[index] = elements[pindex];
-            elements[pindex] = tmp;
-            // 重新赋值
+            if (compare(e, p) <= 0) break;
+            // 将父元素存储在index位置
+            elements[index] = p;
+            // 重新赋值index
             index = pindex;
         }
+        elements[index] = e;
     }
 
     private int compare(E e1, E e2) {
@@ -108,5 +129,32 @@ public class BinaryHeap<E> implements Heap<E> {
     private void elementNotNullCheck(E element) {
         if (element == null)
             throw new IllegalArgumentException("Element must not be null");
+    }
+
+    @Override
+    public Object root() {
+        // 返回的根节点, 就是数组0位置的元素
+        return 0;
+    }
+
+    @Override
+    public Object left(Object node) {
+        // 左索引 2 * index + 1
+        int index = (int) node;
+        index = (index << 1) + 1;
+        // 因为index索引在数组中和size的关系, index从0开始, 所以肯定不能index>=size,越界
+        return index >= size ? null : index;
+    }
+
+    @Override
+    public Object right(Object node) {
+        int index = (int) node;
+        index = (index << 1) + 2;
+        return index >= size ? null : index;
+    }
+
+    @Override
+    public Object string(Object node) {
+        return elements[(int) node];
     }
 }
